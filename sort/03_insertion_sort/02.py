e7ba72bf10a93da024ab717b5c602b5a442674bc
lanzhiwang <hzhilamp@163.com>
2019-12-15 15:36:26 +0800

    插入排序

--- sort/03_insertion_sort/02.py
+++ sort/03_insertion_sort/02.py
@@ -1,41 +1,61 @@
 """
 
 原始序列 99 5 36 7 22 17 46 12  -  从小到大升序排序
-
+     loop_index
 [99, 5, 36, 7, 22, 17, 46, 12]
+
+        loop_index
 [5, 99, 36, 7, 22, 17, 46, 12]
+
+            loop_index
 [5, 36, 99, 7, 22, 17, 46, 12]
 
-1->0
-2->0,1
-3->0,1,2
-4->0,1,2,3
-5->0,1,2,3,4
-6->0,1,2,3,4,5
-7->0,1,2,3,4,5,6
+               loop_index
+[5, 7, 36, 99, 22, 17, 46, 12]
+
+                   loop_index
+[5, 7, 22, 36, 99, 17, 46, 12]
+
+                       loop_index
+[5, 7, 17, 22, 36, 99, 46, 12]
+
+                           loop_index
+[5, 7, 17, 22, 36, 46, 99, 12]
+
+[5, 7, 12, 17, 22, 36, 46, 99]
 
 """
 
 
 def insertion_sort(collection):
     for loop_index in range(1, len(collection)):
-        print('collection: %s' % collection)
-        print('loop_index: %s, collection[%s]: %s' % (loop_index, loop_index, collection[loop_index]))
+        pivot = collection[loop_index]
+
         insertion_index = loop_index - 1
-
         while insertion_index >= 0:
-            print('while')
-            temp = collection[loop_index]
-            print('temp: %s' % temp)
-
-            if collection[insertion_index] > temp:
-                print('break')
-                print('insertion_index: %s, collection[%s]: %s' % (insertion_index, insertion_index, collection[insertion_index]))
+            if collection[insertion_index] > pivot:
+                collection[insertion_index+1] = collection[insertion_index]
+                insertion_index -= 1
+            else:
+                collection[insertion_index + 1] = pivot
                 break
+        else:
+            collection[insertion_index+1] = pivot
+
+
+def insertion_sort_other(collection):
+    for loop_index in range(1, len(collection)):
+        pivot = collection[loop_index]
+
+        insertion_index = loop_index - 1
+        while insertion_index >= 0 and collection[insertion_index] > pivot:
+            collection[insertion_index + 1] = collection[insertion_index]
             insertion_index -= 1
+        else:
+            collection[insertion_index+1] = pivot
 
 
 if __name__ == '__main__':
     unsorted = [99, 5, 36, 7, 22, 17, 46, 12]
-    insertion_sort(unsorted)
+    insertion_sort_other(unsorted)
     print(unsorted)
