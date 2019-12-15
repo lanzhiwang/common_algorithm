5c0411a7e58affdfc265f6098a70bbbb18e62d29
lanzhiwang <hzhilamp@163.com>
2019-12-15 15:11:42 +0800

    快速排序

--- sort/01_quick_sort/04.py
+++ sort/01_quick_sort/04.py
@@ -3,6 +3,59 @@
 
 """
 https://github.com/lanzhiwang/Python-Algorithms/blob/master/sorts/quick_sort_3_partition.py
+
+快速排序 - 从小到大升序排序
+array 6 1 6 2 7 9 3 4 5 10 8
+
+pivot = 6
+i = 0
+6 1 6 2 7 9 3 4 5 10 8
+a                    b
+
+i = 1
+6 1 6 2 7 9 3 4 5 10 8
+a                    b
+
+i = 2
+1 6 6 2 7 9 3 4 5 10 8
+  a                  b
+
+i = 3
+1 6 6 2 7 9 3 4 5 10 8
+  a                  b
+
+i = 4
+1 2 6 6 7 9 3 4 5 10 8
+      a              b
+
+i = 4
+1 2 6 6 8 9 3 4 5 10 7
+      a           b
+
+i = 4
+1 2 6 6 10 9 3 4 5 8 7
+      a          b
+
+i = 4
+1 2 6 6 5 9 3 4 10 8 7
+      a       b
+
+i = 5
+1 2 6 5 6 9 3 4 10 8 7
+        a     b
+
+i = 5
+1 2 6 5 6 4 3 9 10 8 7
+        a   b
+
+i = 5
+1 2 6 5 4 6 3 9 10 8 7
+          a b
+
+i = 6
+1 2 6 5 4 3 6 9 10 8 7
+            ab
+[1, 2, 5, 4, 3, 6, 9, 10, 8, 7]
 """
 def quick_sort_3partition(sorting, left, right):
     if right <= left:
