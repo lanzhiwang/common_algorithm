11c9ea49b0e9daedfed7896f69843e81381ea187
lanzhiwang <hzhilamp@163.com>
2019-12-23 11:10:28 +0800

    常见数据结构-链表

--- data_structures/03_linked_list/02_singly_linked_list.py
+++ data_structures/03_linked_list/02_singly_linked_list.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
 class Node:  # create a Node
     def __init__(self, data):
         self.data = data  # given data
@@ -6,21 +9,43 @@ class Node:  # create a Node
 
 class Linked_List:
     def __init__(self):
-        self.Head = None    # Initialize Head to None
-
-    def insert_tail(self, data):
-        if(self.Head is None): self.insert_head(data)    #If this is first node, call insert_head
-        else:
-            temp = self.Head
-            while(temp.next != None):    #traverse to last node
-                temp = temp.next
-            temp.next = Node(data)    #create node & link to tail
+        self.Head = None  # Initialize Head to None
 
     def insert_head(self, data):
+        """
+        (1, None)
+        ^
+
+        (2, ->) (1, None)
+        ^
+
+        (3, ->) (2, ->) (1, None)
+        ^
+        """
         newNod = Node(data)    # create a new node
         if self.Head != None:
             newNod.next = self.Head     # link newNode to head
         self.Head = newNod    # make NewNode as Head
+
+    def insert_tail(self, data):
+        """
+        (3, ->) (2, ->) (1, None)
+        ^
+
+        (3, ->) (2, ->) (1, ->) (6, None)
+        ^
+
+        (3, ->) (2, ->) (1, ->) (6, ->) (5, ->)
+        ^
+
+        """
+        if self.Head is None:
+            self.insert_head(data)    #If this is first node, call insert_head
+        else:
+            temp = self.Head
+            while temp.next != None:    #traverse to last node
+                temp = temp.next
+            temp.next = Node(data)    #create node & link to tail
 
     def printList(self):  # print every node data
         tamp = self.Head
@@ -29,6 +54,15 @@ class Linked_List:
             tamp = tamp.next
 
     def delete_head(self):  # delete from head
+        """
+        (3, ->) (2, ->) (1, ->) (6, ->) (5, ->)
+        ^
+
+        (2, ->) (1, ->) (6, ->) (5, ->)
+        ^
+        (3, None)
+
+        """
         temp = self.Head
         if self.Head != None:
             self.Head = self.Head.next
@@ -36,9 +70,15 @@ class Linked_List:
         return temp
 
     def delete_tail(self):  # delete from tail
+        """
+        (2, ->) (1, ->) (6, ->) (5, ->)
+        ^
+
+
+        """
         tamp = self.Head
         if self.Head != None:
-            if(self.Head.next is None):    # if Head is the only Node in the Linked List
+            if self.Head.next is None:    # if Head is the only Node in the Linked List
                 self.Head = None
             else:
                 while tamp.next.next is not None:  # find the 2nd last element
@@ -65,34 +105,82 @@ class Linked_List:
         # Return prev in order to put the head at the end
         self.Head = prev
 
+
 def main():
     A = Linked_List()
+
     print("Inserting 1st at Head")
-    a1=input()
+    a1 = input()  # 1
     A.insert_head(a1)
+
     print("Inserting 2nd at Head")
-    a2=input()
+    a2 = input()  # 2
     A.insert_head(a2)
+
     print("\nPrint List : ")
-    A.printList()
+    A.printList()  # 2 1
+
     print("\nInserting 1st at Tail")
-    a3=input()
+    a3 = input()  # 6
     A.insert_tail(a3)
+
     print("Inserting 2nd at Tail")
-    a4=input()
+    a4 = input()  # 5
     A.insert_tail(a4)
+
     print("\nPrint List : ")
-    A.printList()
+    A.printList()  # 2 1 6 5
+
     print("\nDelete Head")
     A.delete_head()
+
     print("Delete Tail")
     A.delete_tail()
+
     print("\nPrint List : ")
-    A.printList()
+    A.printList()  # 1 6
+
     print("\nReverse Linked List")
     A.reverse()
+
     print("\nPrint List : ")
-    A.printList()
+    A.printList()  # 6 1
+
 
 if __name__ == '__main__':
-	main()
+    main()
+
+"""
+Inserting 1st at Head
+1
+Inserting 2nd at Head
+2
+
+Print List :
+2
+1
+
+Inserting 1st at Tail
+6
+Inserting 2nd at Tail
+5
+
+Print List :
+2
+1
+6
+5
+
+Delete Head
+Delete Tail
+
+Print List :
+1
+6
+
+Reverse Linked List
+
+Print List :
+6
+1
+"""
