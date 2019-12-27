fa6396c16fd4d405728eaa72390a512817b606a4
lanzhiwang <hzhilamp@163.com>
2019-12-27 15:50:29 +0800

    平衡二叉树

--- data_structures/01_binary_tree/03_avl_tree/02_avl_tree.py
+++ data_structures/01_binary_tree/03_avl_tree/02_avl_tree.py
@@ -1,5 +1,106 @@
 # -*- coding: utf-8 -*-
 """
-
+https://www.cnblogs.com/vamei/archive/2013/03/21/2964092.html
+https://www.cnblogs.com/suimeng/p/4560056.html
 """
+
+
+def getheight(node):
+    if node is None:
+        return 0
+    return node.getheight()
+
+
+def my_max(a, b):
+    if a > b:
+        return a
+    return b
+
+
+def leftrotation(node):
+    r'''
+            A                      B
+           / \                    / \
+          B   C                  Bl  A
+         / \       -->          /   / \
+        Bl  Br                 UB Br  C
+       /
+     UB
+
+    UB = unbalanced node
+
+    node = A
+    ret = B
+    '''
+    print("left rotation node:", node.getdata())
+    ret = node.getleft()
+    node.setleft(ret.getright())
+    ret.setright(node)
+    h1 = my_max(getheight(node.getright()), getheight(node.getleft())) + 1
+    node.setheight(h1)
+    h2 = my_max(getheight(ret.getright()), getheight(ret.getleft())) + 1
+    ret.setheight(h2)
+    return ret
+
+
+def rightrotation(node):
+    r'''
+        A                      C
+       / \                    / \
+      B   C                  A   Cr
+         / \       -->      / \   \
+        Cl  Cr             B  Cl  UB
+             \
+             UB
+
+    UB = unbalanced node
+
+    node = A
+    ret = C
+    '''
+    print("right rotation node:", node.getdata())
+    ret = node.getright()
+    node.setright(ret.getleft())
+    ret.setleft(node)
+    h1 = my_max(getheight(node.getright()), getheight(node.getleft())) + 1
+    node.setheight(h1)
+    h2 = my_max(getheight(ret.getright()), getheight(ret.getleft())) + 1
+    ret.setheight(h2)
+    return ret
+
+
+def rlrotation(node):
+    r'''
+            A              A                    Br
+           / \            / \                  /  \
+          B   C    RR    Br  C       LR       B    A
+         / \       -->  /  \         -->    /     / \
+        Bl  Br         B   UB              Bl    UB  C
+             \        /
+             UB     Bl
+    RR = rightrotation   LR = leftrotation
+
+    node = A
+    '''
+    node.setleft(rightrotation(node.getleft()))
+    return leftrotation(node)
+
+
+def lrrotation(node):
+    r'''
+        A                   A                    Cl
+       / \                 / \                   / \
+      B   C               B  Cl                 A   C
+         / \    -->          / \    -->        / \   \
+        Cl  Cr              UB  C             B  UB  Cr
+        /                        \
+       UB                        Cr
+
+    UB = unbalanced node
+
+    node = A
+    '''
+    node.setright(leftrotation(node.getright()))
+    return rightrotation(node)
+
 
--- data_structures/01_binary_tree/README.md
+++ data_structures/01_binary_tree/README.md
@@ -5,6 +5,8 @@
 * 二叉搜索树（二叉查找树）
 
 * 平衡查找树（防止二叉查找树退化为线性列表）
+	* AVL 树
+
 	* 2-3查找树
 
 	* 红黑树
@@ -16,6 +18,11 @@
 
 * https://www.cnblogs.com/yangecnu/p/introduce-2-3-search-tree.html
 
+## 自平衡二叉查找树（AVL）
+
+* https://blog.csdn.net/lpjishu/article/details/52130384
+
+
 ## 红黑树
 
 * https://github.com/lanzhiwang/awesome-huzhi/blob/master/algorithm/Tree.md#%E7%BA%A2%E9%BB%91%E6%A0%91
@@ -26,3 +33,5 @@
 
 
 ## B 树
+
+
