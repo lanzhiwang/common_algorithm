93e5dccf6c33072877e07b35ff5a5b7c583b2aa8
lanzhiwang <hzhilamp@163.com>
2020-01-04 20:54:22 +0800

    avl 平衡二叉树

--- data_structures/01_binary_tree/03_avl_tree/03_avl_tree.py
+++ data_structures/01_binary_tree/03_avl_tree/03_avl_tree.py
@@ -37,98 +37,21 @@ def insert_node(node, data):
     if node is None:
         return my_node(data)
 
-    left_node = node.getleft()
-    right_node = node.getright()
     if data < node.getdata():
-        new_node = insert_node(left_node, data)
-        node.setleft(new_node)
-        if getheight(left_node) - getheight(right_node) == 2:
-            if data < left_node.getdata():
+        node.setleft(insert_node(node.getleft(), data))
+        if getheight(node.getleft()) - getheight(node.getright()) == 2:  # an unbalance detected
+            if data < node.getleft().getdata():  # new node is the left child of the left child
                 node = leftrotation(node)
             else:
-                node = rlrotation(node)
+                node = rlrotation(node)  # new node is the right child of the left child
     else:
-        new_node = insert_node(right_node, data)
-        node.setright(new_node)
-        if getheight(right_node) - getheight(left_node) == 2:
-            if data < right_node.getdata():
+        node.setright(insert_node(node.getright(), data))
+        if getheight(node.getright()) - getheight(node.getleft()) == 2:
+            if data < node.getright().getdata():
                 node = lrrotation(node)
             else:
                 node = rightrotation(node)
 
-    h = my_max(getheight(node.getleft()), getheight(node.getright())) + 1
-    node.setheight(h)
+    h1 = my_max(getheight(node.getright()), getheight(node.getleft())) + 1
+    node.setheight(h1)
     return node
-
-"""
-[1, 7, 4, 6, 5, 3, 8, 0, 2, 9]
-
-
-[3, 9, 5, 0, 2, 7, 8, 4, 1, 6]
-
-
-[6, 2, 3, 1, 5, 0, 4, 9, 7, 8]
-
-
-
-[3, 6, 4, 2, 0, 9, 8, 5, 1, 7]
-
-
-
-[4, 2, 7, 6, 3, 5, 8, 0, 9, 1]
-insert:4
- 4
-*************************************
-insert:2
-  4
- 2  *
-*************************************
-insert:7
-  4
- 2  7
-*************************************
-insert:6
-    4
-  2    7
- *  *  6  *
-*************************************
-insert:3
-    4
-  2    7
- *  3  6  *
-*************************************
-insert:5
-left rotation node: 7
-    4
-  2    6
- *  3  5  7
-*************************************
-insert:8
-        4
-    2        6
-  *    3    5    7
- *  *  *  *  *  *  *  8
-*************************************
-insert:0
-        4
-    2        6
-  0    3    5    7
- *  *  *  *  *  *  *  8
-*************************************
-insert:9
-right rotation node: 7
-        4
-    2        6
-  0    3    5    8
- *  *  *  *  *  *  7  9
-*************************************
-insert:1
-        4
-    2        6
-  0    3    5    8
- *  1  *  *  *  *  7  9
-*************************************
-
-
-
-"""
--- data_structures/01_binary_tree/03_avl_tree/04_avl_tree.py
+++ data_structures/01_binary_tree/03_avl_tree/04_avl_tree.py
@@ -12,19 +12,51 @@ def getLeftMost(root):
     pass
 
 
+def my_max(a, b):
+    pass
+
+
+class my_node:
+    pass
+
+
+def getheight(node):
+    pass
+
+
+def leftrotation(node):
+    pass
+
+
+def rlrotation(node):
+    pass
+
+
+def lrrotation(node):
+    pass
+
+
+def rightrotation(node):
+    pass
+
 
 # del_node(self.root, data)
 def del_node(root, data):
-    root_data = root.getdata()
-    if data == root_data:
+    if data == root.getdata():
+        """
+        left     | right
+        ---------|---------
+        not None | not None
+        not None | None
+        None     | not None
+        None     | None
+        """
         if root.getleft() is not None and root.getright() is not None:
             temp_data = getLeftMost(root.getright())
             root.setdata(temp_data)
             root.setright(del_node(root.getright(), temp_data))
-
         elif root.getleft() is not None:
             root = root.getleft()
-
         else:
             root = root.getright()
 
@@ -33,18 +65,30 @@ def del_node(root, data):
             print("No such data")
             return root
         else:
-            root.setleft(del_node(root.getleft(),data))
+            root.setleft(del_node(root.getleft(), data))
 
     elif data > root.getdata():
         if root.getright() is None:
             return root
         else:
-            root.setright(del_node(root.getright(),data))
-
-
+            root.setright(del_node(root.getright(), data))
 
     if root is None:
         return root
+
+    r"""
+     6           6           6      6
+      \           \           \      \
+       9  =>       9           9      9
+     /  \           \         /      / \
+     8  10           10      8       8 10
+
+         6             6        6           6
+        /             /        /           /
+       3   =>        3        3           3
+      / \           /          \         / \
+     2   5         2            5       2   5
+    """
     if getheight(root.getright()) - getheight(root.getleft()) == 2:
         if getheight(root.getright().getright()) > getheight(root.getright().getleft()):
             root = rightrotation(root)
@@ -55,6 +99,10 @@ def del_node(root, data):
             root = leftrotation(root)
         else:
             root = rlrotation(root)
-    height = my_max(getheight(root.getright()),getheight(root.getleft())) + 1
+
+    height = my_max(getheight(root.getright()), getheight(root.getleft())) + 1
     root.setheight(height)
     return root
+
+
+
