eb956a9ec85803e5b70cfef66821e1649900c596
lanzhiwang <hzhilamp@163.com>
2020-01-07 12:43:59 +0800

    栈 - 先进后出

--- data_structures/05_stacks/__init__.py
+++ data_structures/05_stacks/__init__.py
@@ -1,3 +1,7 @@
+"""
+栈 - 先进后出
+"""
+
 class Stack:
 
    def __init__(self):
@@ -5,7 +9,7 @@ class Stack:
       self.top = 0
 
    def is_empty(self):
-      return (self.top == 0)
+      return self.top == 0
 
    def push(self, item):
       if self.top < len(self.stack):
@@ -21,3 +25,23 @@ class Stack:
       else:
          self.top -= 1
          return self.stack[self.top]
+
+   def __str__(self):
+      return 'stack: %s, top: %s' % (self.stack, self.top)
+
+
+if __name__ == '__main__':
+   s = Stack()
+   print(s)  # stack: [], top: 0
+
+   s.push(1)
+   s.push(2)
+   s.push(3)
+   s.push(4)
+   print(s)  # stack: [1, 2, 3, 4], top: 4
+
+   print(s.pop())  # 4
+   print(s)  # stack: [1, 2, 3, 4], top: 3
+
+   s.push(5)
+   print(s)  # stack: [1, 2, 3, 5], top: 4
