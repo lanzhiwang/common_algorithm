2ab5f8e3bdb22ea84089851dcecc9a339a3ba429
lanzhiwang <hzhilamp@163.com>
2020-01-08 17:28:27 +0800

    动态规划

--- dynamic_programming/01.py
+++ dynamic_programming/01.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """
 浅谈什么是动态规划以及相关的「股票」算法题
 https://mp.weixin.qq.com/s/p91e-EuSuVK3bfOc7uJplg
@@ -7,5 +10,7 @@ https://mp.weixin.qq.com/s/zKjULzepeBRlQLJoJHynXQ
 
 有了四步解题法模板，再也不害怕动态规划！
 https://mp.weixin.qq.com/s/RPBRysbX7wgW_vnPNqloYA
+
+https://juejin.im/post/5d88f5366fb9a06afd6644c7
 
 """
