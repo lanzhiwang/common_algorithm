99665322595ae52e27d52b03c26457158ae4b49a
lanzhiwang <hzhilamp@163.com>
2020-01-08 13:46:55 +0800

    动态规划

--- dynamic_programming/01.py
+++ dynamic_programming/01.py
@@ -1,9 +1,11 @@
 """
-
+浅谈什么是动态规划以及相关的「股票」算法题
 https://mp.weixin.qq.com/s/p91e-EuSuVK3bfOc7uJplg
 
+动态规划之空间优化与总结回顾
 https://mp.weixin.qq.com/s/zKjULzepeBRlQLJoJHynXQ
 
+有了四步解题法模板，再也不害怕动态规划！
 https://mp.weixin.qq.com/s/RPBRysbX7wgW_vnPNqloYA
 
 """
