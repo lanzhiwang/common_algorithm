2d07131a3eb49e280314aa4be7af8615430f0f04
lanzhiwang <hzhilamp@163.com>
2020-01-10 15:05:42 +0800

    归并排序

--- sort/07_merge_sort/02.py
+++ sort/07_merge_sort/02.py
@@ -9,27 +9,34 @@ collection: [6], start: [2, 3, 5], end: [10, 8, 7]
 
 
 def merge_sort(collection):
+    if len(collection) <= 1:
+        return collection
+
     start, end = [], []
     while len(collection) > 1:
-        print('collection: %s, start: %s, end: %s' % (collection, start, end))
+        # print('collection: %s, start: %s, end: %s' % (collection, start, end))
         min_one, max_one = min(collection), max(collection)
         start.append(min_one)
         end.append(max_one)
         collection.remove(min_one)
         collection.remove(max_one)
-    print('collection: %s, start: %s, end: %s' % (collection, start, end))
+    # print('collection: %s, start: %s, end: %s' % (collection, start, end))
 
     end.reverse()
     return start + collection + end
 
 
 if __name__ == '__main__':
-    user_input = '10, 5, 7, 3, 2, 8, 6'
-    unsorted = [int(item) for item in user_input.split(',')]
-    print(*merge_sort(unsorted), sep=',')
+    unsorted = [10, 5, 7, 3, 2, 8, 6]
+    print(merge_sort(unsorted))
 
-
-
-
-
-
+    for unsorted in [
+        [], [0], [2], [3, 5], [5, 3], [5, 5], [0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 3, 5],
+        [2, 5, 3, 0, 2, 3, 0, 3], [0, 2, 2, 3, 5],
+        [103, 9, 1, 7, 11, 15, 25, 201, 209, 107, 5],
+        [6, 1, 2, 7, 9, 3, 4, 5, 10, 8],
+        [-45, -2, -5]
+    ]:
+        print(unsorted)
+        print(merge_sort(unsorted))
+        print()
