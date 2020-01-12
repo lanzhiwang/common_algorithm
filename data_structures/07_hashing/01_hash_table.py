f512eec1a636dc3a667af89221cd66cfacf1f49a
lanzhiwang <hzhilamp@163.com>
2020-01-12 17:21:11 +0800

    hash table

--- data_structures/07_hashing/01_hash_table.py
+++ data_structures/07_hashing/01_hash_table.py
@@ -44,7 +44,9 @@ class HashTable:
         self._keys[key] = data
 
     def _colision_resolution(self, key, data=None):
+        # key: 0, data: 10
         new_key = self.hash_function(key + 1)
+        print('new_key: %s' % new_key)  # 1
 
         while self.values[new_key] is not None \
                 and self.values[new_key] != key:
@@ -65,7 +67,13 @@ class HashTable:
         map(self.insert_data, survivor_values)
 
     def insert_data(self, data):
+        print('data: %s' % data)  # 10
         key = self.hash_function(data)
+        print('key: %s' % key)  # 0
+
+        print('values: %s \n_keys: %s' % (self.values, self._keys))
+        # values: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        # _keys: {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
 
         if self.values[key] is None:
             self._set_value(key, data)
@@ -75,10 +83,31 @@ class HashTable:
 
         else:
             colision_resolution = self._colision_resolution(key, data)
+            print('colision_resolution: %s' % colision_resolution)  # None
             if colision_resolution is not None:
                 self._set_value(colision_resolution, data)
             else:
                 self.rehashing()
                 self.insert_data(data)
+        print('values: %s \n_keys: %s' % (self.values, self._keys))
+        print()
+
+    def __str__(self):
+        return 'size_table: %s \nvalues: %s\nlim_charge: %s \ncharge_factor: %s \
+               \n__aux_list: %s \n_keys: %s' % (self.size_table, self.values, \
+               self.lim_charge, self.charge_factor, self.__aux_list, self._keys)
+
+
+if __name__ == '__main__':
+    hash_table = HashTable(10)
+    print(hash_table)
+    print()
+    for i in range(5):
+        hash_table.insert_data(i)
+    print()
+    hash_table.insert_data(10)
+    print()
+    print(hash_table)
+
 
 
--- data_structures/07_hashing/number_theory/prime_numbers.py
+++ data_structures/07_hashing/number_theory/prime_numbers.py
@@ -7,6 +7,21 @@
 def check_prime(number):
         """
             it's not the best solution
+
+            number = 0 -> 2
+            number = 1 -> 2
+            number = 2 -> 3
+
+            >>> number = 20
+            >>> l1 = [i for i in range(2, number)]
+            >>> l1
+            [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+            >>> l2 = [number % i for i in l1]
+            >>> l2
+            [0, 2, 0, 0, 2, 6, 4, 2, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+            >>> all(l2)
+            False
+            >>>
         """
         special_non_primes = [0,1,2]
         if number in special_non_primes[:2]:
@@ -18,8 +33,8 @@ def check_prime(number):
 
 
 def next_prime(value, factor=1, **kwargs):
-    value = factor * value
-    first_value_val = value
+    value = factor * value  # 2 * 10
+    first_value_val = value  # 10
     
     while not check_prime(value):
         value += 1 if not ("desc" in kwargs.keys() and kwargs["desc"] is True) else -1
@@ -27,3 +42,17 @@ def next_prime(value, factor=1, **kwargs):
     if value == first_value_val:
         return next_prime(value + 1, **kwargs)
     return value
+
+
+
+if __name__ == '__main__':
+    print(check_prime(0))  # 2
+    print(check_prime(1))  # 2
+    print(check_prime(2))  # 3
+    print(check_prime(3))  # True
+    print(check_prime(4))  # False
+    print(check_prime(5))  # True
+    print(check_prime(20))  # False
+
+    print(next_prime(10, 2))  # 23
+    
