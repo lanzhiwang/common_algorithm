9a190d3f11b450eff9960f6fa49eff5e0c226632
胡志 <huzhi@huzhideMacBook-Pro.local>
2020-01-23 12:17:06 +0800

    基本树结构

--- data_structures/01_binary_tree/01_basic_binary_tree.py
+++ data_structures/01_binary_tree/01_basic_binary_tree.py
@@ -1,11 +1,15 @@
-class Node: # This is the Class Node with constructor that contains data variable to type data and left,right pointers.
+class Node:
+    """This is the Class Node with constructor that contains data variable to type data and left,right pointers.
+    """
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-def display(tree): #In Order traversal of the tree
 
+def display(tree):
+    """In Order traversal of the tree
+    """
     if tree is None: 
         return
 
@@ -19,7 +23,10 @@ def display(tree): #In Order traversal of the tree
 
     return
 
-def depth_of_tree(tree): #This is the recursive function to find the depth of binary tree.
+
+def depth_of_tree(tree):
+    """This is the recursive function to find the depth of binary tree.
+    """
     if tree is None:
         return 0
     else:
@@ -31,20 +38,23 @@ def depth_of_tree(tree): #This is the recursive function to find the depth of bi
             return 1 + depth_r_tree
 
 
-def is_full_binary_tree(tree): # This functions returns that is it full binary tree or not?
+def is_full_binary_tree(tree):
+    """This functions returns that is it full binary tree or not?
+    """
     if tree is None:
         return True
+
     if (tree.left is None) and (tree.right is None):
         return True
 
     if (tree.left is not None) and (tree.right is not None):
-        return (is_full_binary_tree(tree.left) and is_full_binary_tree(tree.right))
+        return is_full_binary_tree(tree.left) and is_full_binary_tree(tree.right)
     else:
         return False
 
 
-def main(): # Main func for testing.
-    r"""
+def main():
+    r"""Main func for testing.
 
          1
       /    \
