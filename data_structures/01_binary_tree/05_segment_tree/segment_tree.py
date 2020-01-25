c99d48bcb7269f6287610e92257aa2b7c9a957f6
lanzhiwang <hzhilamp@163.com>
2020-01-25 22:05:36 +0800

    汇总线段树

--- data_structures/01_binary_tree/05_segment_tree/segment_tree.py
+++ data_structures/01_binary_tree/05_segment_tree/segment_tree.py
@@ -1,74 +1,249 @@
-"""
-Segment Tree 线段树
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-"""
-import math
+from queue import Queue
 
-class SegmentTree:
 
-    def __init__(self, A):
-        self.N = len(A)
-        self.st = [0] * (4 * self.N) # approximate the overall size of segment tree with array N
-        self.build(1, 0, self.N - 1)
+class SegmentTreeNode(object):
+    def __init__(self, start, end, val, left=None, right=None):
+        self.start = start
+        self.end = end
+        self.val = val
+        self.mid = (start + end) // 2
+        self.left = left
+        self.right = right
 
-    def left(self, idx):
-        return idx * 2
+    def __str__(self):
+        return 'val: %s, start: %s, end: %s' % (self.val, self.start, self.end)
 
-    def right(self, idx):
-        return idx * 2 + 1
 
-    def build(self, idx, l, r):
-        if l == r:
-            self.st[idx] = A[l]
+class NumArray:
+    def __init__(self, nums):
+        self.nums = nums
+        if self.nums:
+            self.root = self._build_tree(0, len(nums) - 1)
+
+    def update(self, i, val):
+        self._update_tree(self.root, i, val)
+
+    def sum_range(self, i, j):
+        return self._sum_range(self.root, i, j)
+
+    def _build_tree(self, start, end):
+        if start == end:
+            return SegmentTreeNode(start, end, self.nums[start])
+        mid = (start + end) // 2
+        left = self._build_tree(start, mid)
+        right = self._build_tree(mid + 1, end)
+        return SegmentTreeNode(start, end, left.val + right.val, left, right)
+
+    def _update_tree(self, root, i, val):
+        if root.start == i and root.end == i:
+            root.val = val
+            return
+        if i <= root.mid:
+            self._update_tree(root.left, i, val)
         else:
-            mid = (l + r) // 2
-            self.build(self.left(idx), l, mid)
-            self.build(self.right(idx), mid + 1, r)
-            self.st[idx] = max(self.st[self.left(idx)] , self.st[self.right(idx)])
+            self._update_tree(root.right, i, val)
+        root.val = root.left.val + root.right.val
 
-    def update(self, a, b, val):
-        return self.update_recursive(1, 0, self.N - 1, a - 1, b - 1, val)
+    def _sum_range(self, root, i, j):
+        if root.start == i and root.end == j:
+            return root.val
+        """
+         [i, j] [i, j] [i, j]
+        [start mid] [mid+1 end]
+        """
+        if j <= root.mid:
+            return self._sum_range(root.left, i, j)
+        elif i > root.mid:
+            return self._sum_range(root.right, i, j)
+        else:
+            return self._sum_range(root.left, i, root.mid) + self._sum_range(root.right, root.mid + 1, j)
 
-    def update_recursive(self, idx, l, r, a, b, val): # update(1, 1, N, a, b, v) for update val v to [a,b]
-        if r < a or l > b:
-            return True
-        if l == r :
-            self.st[idx] = val
-            return True
-        mid = (l+r)//2
-        self.update_recursive(self.left(idx), l, mid, a, b, val)
-        self.update_recursive(self.right(idx), mid+1, r, a, b, val)
-        self.st[idx] = max(self.st[self.left(idx)] , self.st[self.right(idx)])
-        return True
+    def traverse(self):
+        result = []
+        if self.root is not None:
+            queue = Queue()
+            queue.put(self.root)
+            while not queue.empty():
+                node = queue.get()
+                result.append(node)
 
-    def query(self, a, b):
-        return self.query_recursive(1, 0, self.N - 1, a - 1, b - 1)
+                if node.left is not None:
+                    queue.put(node.left)
 
-    def query_recursive(self, idx, l, r, a, b): #query(1, 1, N, a, b) for query max of [a,b]
-        if r < a or l > b:
-            return -math.inf
-        if l >= a and r <= b:
-            return self.st[idx]
-        mid = (l+r)//2
-        q1 = self.query_recursive(self.left(idx), l, mid, a, b)
-        q2 = self.query_recursive(self.right(idx), mid + 1, r, a, b)
-        return max(q1, q2)
+                if node.right is not None:
+                    queue.put(node.right)
+            return result
 
-    def showData(self):
-        showList = []
-        for i in range(1,N+1):
-            showList += [self.query(i, i)]
-        print(showList)
+
+class MaxArray:
+    def __init__(self, nums):
+        self.nums = nums
+        if self.nums:
+            self.root = self._build_tree(0, len(nums) - 1)
+
+    def update(self, i, val):
+        self._update_tree(self.root, i, val)
+
+    def max_range(self, i, j):
+        return self._max_range(self.root, i, j)
+
+    def _build_tree(self, start, end):
+        if start == end:
+            return SegmentTreeNode(start, end, self.nums[start])
+        mid = (start + end) // 2
+        left = self._build_tree(start, mid)
+        right = self._build_tree(mid + 1, end)
+        return SegmentTreeNode(start, end, max([left.val, right.val]), left, right)
+
+    def _update_tree(self, root, i, val):
+        if root.start == i and root.end == i:
+            root.val = val
+            return
+        if i <= root.mid:
+            self._update_tree(root.left, i, val)
+        else:
+            self._update_tree(root.right, i, val)
+        root.val = max([root.left.val, root.right.val])
+
+    def _max_range(self, root, i, j):
+        if root.start == i and root.end == j:
+            return root.val
+        """
+         [i, j] [i, j] [i, j]
+        [start mid] [mid+1 end]
+        """
+        if j <= root.mid:
+            return self._max_range(root.left, i, j)
+        elif i > root.mid:
+            return self._max_range(root.right, i, j)
+        else:
+            return max([self._max_range(root.left, i, root.mid), self._max_range(root.right, root.mid + 1, j)])
+
+    def traverse(self):
+        result = []
+        if self.root is not None:
+            queue = Queue()
+            queue.put(self.root)
+            while not queue.empty():
+                node = queue.get()
+                result.append(node)
+
+                if node.left is not None:
+                    queue.put(node.left)
+
+                if node.right is not None:
+                    queue.put(node.right)
+            return result
+
+
+class MinArray:
+    def __init__(self, nums):
+        self.nums = nums
+        if self.nums:
+            self.root = self._build_tree(0, len(nums) - 1)
+
+    def update(self, i, val):
+        self._update_tree(self.root, i, val)
+
+    def min_range(self, i, j):
+        return self._min_range(self.root, i, j)
+
+    def _build_tree(self, start, end):
+        if start == end:
+            return SegmentTreeNode(start, end, self.nums[start])
+        mid = (start + end) // 2
+        left = self._build_tree(start, mid)
+        right = self._build_tree(mid + 1, end)
+        return SegmentTreeNode(start, end, min([left.val, right.val]), left, right)
+
+    def _update_tree(self, root, i, val):
+        if root.start == i and root.end == i:
+            root.val = val
+            return
+        if i <= root.mid:
+            self._update_tree(root.left, i, val)
+        else:
+            self._update_tree(root.right, i, val)
+        root.val = min([root.left.val, root.right.val])
+
+    def _min_range(self, root, i, j):
+        if root.start == i and root.end == j:
+            return root.val
+        """
+         [i, j] [i, j] [i, j]
+        [start mid] [mid+1 end]
+        """
+        if j <= root.mid:
+            return self._min_range(root.left, i, j)
+        elif i > root.mid:
+            return self._min_range(root.right, i, j)
+        else:
+            return min([self._min_range(root.left, i, root.mid), self._min_range(root.right, root.mid + 1, j)])
+
+    def traverse(self):
+        result = []
+        if self.root is not None:
+            queue = Queue()
+            queue.put(self.root)
+            while not queue.empty():
+                node = queue.get()
+                result.append(node)
+
+                if node.left is not None:
+                    queue.put(node.left)
+
+                if node.right is not None:
+                    queue.put(node.right)
+            return result
 
 
 if __name__ == '__main__':
-    A = [1,2,-4,7,3,-5,6,11,-20,9,14,15,5,2,-8]
-    N = 15
-    segt = SegmentTree(A)
-    print(segt.query(4, 6))
-    print(segt.query(7, 11))
-    print(segt.query(7, 12))
-    segt.update(1,3,111)
-    print(segt.query(1, 15))
-    segt.update(7,8,235)
-    segt.showData()
+    print('求和线段树')
+    num_arr = NumArray([2, 1, 5, 3, 4])
+    for node in num_arr.traverse():
+        print(node)
+    print()
+
+    num_arr.update(1, 5)
+    for node in num_arr.traverse():
+        print(node)
+    print()
+
+    print(num_arr.sum_range(3, 4))  # 7
+    print(num_arr.sum_range(2, 2))  # 5
+    print(num_arr.sum_range(1, 3))  # 13
+
+    print()
+    print('求最大值线段树')
+    max_arr = MaxArray([2, 1, 5, 3, 4])
+    for node in max_arr.traverse():
+        print(node)
+    print()
+
+    max_arr.update(1, 5)
+    for node in max_arr.traverse():
+        print(node)
+    print()
+
+    print(max_arr.max_range(3, 4))  # 4
+    print(max_arr.max_range(2, 2))  # 5
+    print(max_arr.max_range(1, 3))  # 5
+
+    print()
+    print('求最小值线段树')
+    min_arr = MinArray([2, 1, 5, 3, 4])
+    for node in min_arr.traverse():
+        print(node)
+    print()
+
+    min_arr.update(1, 5)
+    for node in min_arr.traverse():
+        print(node)
+    print()
+
+    print(min_arr.min_range(3, 4))  # 3
+    print(min_arr.min_range(2, 2))  # 5
+    print(min_arr.min_range(1, 3))  # 3
